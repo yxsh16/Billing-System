339abae1bd18742be520250b4d49e0579881d0b3
yash16jr <yash16jr@gmail.com>
2024-02-06 19:05:57 +0530

    Added billing

--- config/settings/base.py
+++ config/settings/base.py
@@ -81,6 +81,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "billing_sys.users",
+    "billing_sys.billing"
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
