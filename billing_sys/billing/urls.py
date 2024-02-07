ebeec4f66bd5d5f2c08a458dc554a55e73b79315
yash16jr <yash16jr@gmail.com>
2024-02-07 23:00:14 +0530

    stripe-integration-initate

--- billing_sys/billing/urls.py
+++ billing_sys/billing/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from billing_sys.billing.views import billing_home
+from billing_sys.billing.views import billing_home, checkout_session
 
 app_name = "billing"
 
 urlpatterns = [
     path("", view=billing_home, name="home"),
+    path("checkout", view=checkout_session, name="checkout")
 ]
--- billing_sys/billing/views.py
+++ billing_sys/billing/views.py
@@ -1,7 +1,30 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+import stripe
 
 # Create your views here.
 
 def billing_home(request: HttpRequest):
     return render(request, template_name = 'billing/billing_home.html')
+
+
+def checkout_session(request: HttpRequest):
+    if request.method == "POST":
+        session = stripe.checkout.Session.create(
+    line_items=[{
+      'price_data': {
+        'currency': 'usd',
+        'product_data': {
+          'name': 'T-shirt',
+        },
+        'unit_amount': 2000,
+      },
+      'quantity': 1,
+    }],
+    mode='payment',
+    success_url='http://localhost:4242/success',
+    cancel_url='http://localhost:4242/cancel',
+  )
+
+        return redirect(session.url, code=303)
+    return render(request, template_name= 'billing/checkout_session.html')
