9fb70298afcff689ffeafd161e13ec6f33cba8a3
yash16jr <yash16jr@gmail.com>
2024-02-11 20:39:01 +0530

    billing process complete

--- billing_sys/billing/views.py
+++ billing_sys/billing/views.py
@@ -1,39 +1,42 @@
-from django.http import HttpRequest
-from django.conf import settings
-from django.shortcuts import redirect, render
-from django.urls import reverse
 import stripe
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 # Create your views here.
 
+@login_required
 def billing_home(request: HttpRequest):
     return render(request, template_name = 'billing/billing_home.html')
 
-
+@login_required
 def checkout_session(request: HttpRequest):
-    if request.method == "POST":
-        stripe.api_key=settings.STRIPE_SECRET_KEY
+    if request.method == 'POST':
+        stripe.api_key = settings.STRIPE_SECRET_KEY
         if request.user.stripe_customer_id:
             customer_id = request.user.stripe_customer_id
         else:
             customer = stripe.Customer.create(
-                email=request.user.email
-            )    
+                email=request.user.email,
+            )
             customer_id = customer['id']
             request.user.stripe_customer_id = customer_id
             request.user.save()
-            
         session = stripe.checkout.Session.create(
-            mode="setup",
-            currency="inr",
-            customer = customer_id,
-            success_url=request.build_absolute_uri(reverse('billing:checkout_success')) + '?session_id={CHECKOUT_SESSION_ID}',
-            cancel_url="https://example.com/cancel",
+            payment_method_types=['card'],
+            mode='setup',
+            customer=customer_id,
+            success_url=request.build_absolute_uri(
+                reverse('billing:checkout_success')) + '?session_id={CHECKOUT_SESSION_ID}',
+            cancel_url='https://example.com/cancel',
         )
+        
         return redirect(session.url, code=303)
-    return render(request, template_name= 'billing/checkout_session.html')
+    return render(request, 'billing/checkout_session.html')
 
-
+@login_required
 def checkout_success(request: HttpRequest):
     session_id = request.GET.get('session_id')
     session = stripe.checkout.Session.retrieve(session_id)
